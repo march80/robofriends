a41b35acaf1fa83bebd529d52bbb0173f333716e
Matt March <matt.marchcoding@gmail.com>
2024-01-24 17:43:46 -0500

    removing count button

--- src/containers/App.js
+++ src/containers/App.js
@@ -37,7 +37,7 @@ function App () {
             (
             <div className="tc">
                 <h1 className="f1">RoboFriends</h1>
-                <button onClick={() => setCount(count+1)}>Click Me!</button>
+                
                 <SearchBox searchChange={onSearchChange}/>
                 <Scroll>
                     <ErrorBoundry>
